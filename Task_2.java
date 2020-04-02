19cbe154a81f7d9ade2e337ed8e7b9aa02483e00
Artem Sharov <sharov.a.p.10@gmail.com>
2020-04-02 21:13:45 +0300

    task_2 done

--- Task_2.java
+++ Task_2.java
@@ -1,4 +1,19 @@
 package ru.gb.java_3.hw_1;
 
+import java.util.ArrayList;
+
+//Написать метод, который преобразует массив в ArrayList;
 public class Task_2 {
+    public static <T> T arrList(T arr[]){
+        ArrayList <T> aList = new ArrayList();
+        for (int i = 0; i <arr.length; i++) {
+            aList.add(arr[i]);
+        }
+        return (T) aList;
+    }
+
+    public static void main(String[] args) {
+        Integer a[] = {1,2,3,4,5,6};
+        System.out.println(arrList(a));
+    }
 }
